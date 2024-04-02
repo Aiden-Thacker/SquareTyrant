2580ba0fae6667e3b1d881111bb076dd0495eced
Liam York <yorkliam08@gmail.com>
2024-04-02 16:44:06 -0500

    Made Test_Scene

---
+++ core/src/com/squaretyrant/game/Test_Scene.java
@@ -0,0 +1,49 @@
+package com.squaretyrant.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Test_Scene extends ApplicationAdapter {
+
+    private Skin skin;
+    private Stage stage;
+
+    @Override
+    public void create () {
+        skin = new Skin(Gdx.files.internal("metalui/metal-ui.json"));
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        //Begin layout
+        TextButton textButton = new TextButton("Hello", skin);
+        stage.addActor(textButton);
+    }
+
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(.9f, .9f, .9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose () {
+        skin.dispose();
+        stage.dispose();
+    }
+}
+
+
